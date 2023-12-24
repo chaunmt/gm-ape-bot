01a0d4effb252ef8e8b59203fb1e29aeb8349912
Chau Truong <37993386+chaunmt@users.noreply.github.com>
2023-12-24 14:14:17 -0600

    ping pong is played and random activity

--- src/main/java/GMBot.java
+++ src/main/java/GMBot.java
@@ -3,10 +3,8 @@ import net.dv8tion.jda.api.JDABuilder;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.events.GenericEvent;
-import net.dv8tion.jda.api.events.session.ReadyEvent;
 
-import java.util.EventListener;
+import java.util.*;
 
 public class GMBot implements EventListener
 {
@@ -16,11 +14,31 @@ public class GMBot implements EventListener
         Dotenv dotenv = Dotenv.load();                                  // Load API
         String GM_APE_TOKEN = dotenv.get("GM_APE_TOKEN");
 
+        generateActivities();
         JDA jda = JDABuilder.createDefault(GM_APE_TOKEN)                // Create Bot
                 .addEventListeners(new SlashCommandHandler())           // Slash Command
-                .setActivity(Activity.playing(" with dj ape"))
+                .setActivity(randomActivity())
                 .build();                                               // Build Bot
 
-//        jda.awaitReady();   // Wait until jda is ready
+        jda.awaitReady();   // Wait until jda is ready
+    }
+
+    private static final ArrayList<Activity> activities = new ArrayList<>();
+
+    private static void generateActivities() {
+        activities.add(Activity.watching("Planet of Apes"));
+        activities.add(Activity.playing("Jenshin"));
+        activities.add(Activity.playing("HSR"));
+        activities.add(Activity.playing("with DJ Ape"));
+        activities.add(Activity.playing("with banana"));
+        activities.add(Activity.playing("with banana"));
+        activities.add(Activity.competing("with DJ Ape"));
+    }
+
+    private static Activity randomActivity()
+    {
+        Random rand = new Random();
+        int length = activities.size();
+        return activities.get(rand.nextInt(length));
     }
 }
--- src/main/java/SlashCommandHandler.java
+++ src/main/java/SlashCommandHandler.java
@@ -1,68 +1,34 @@
-import io.github.cdimascio.dotenv.Dotenv;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
+import java.time.Instant;
+
+import static java.lang.Math.abs;
 
 public class SlashCommandHandler extends ListenerAdapter
 {
-    public static void main(String[] args)
-    {
-        Dotenv dotenv = Dotenv.load();                                  // Load API
-        String GM_APE_TOKEN = dotenv.get("GM_APE_TOKEN");
-
-        JDA jda = JDABuilder.createDefault(GM_APE_TOKEN)                // Create Bot
-                .addEventListeners(new SlashCommandHandler())           // Slash Command
-                .build();
-
-        jda.updateCommands()
+    public void onReady(ReadyEvent event) {
+        event.getJDA().updateCommands()
                 .addCommands(
-                        Commands.slash("ping", "Ping?"),
-                        Commands.slash("pong", "Pong?")
+                        Commands.slash("ping", "Ping?")
                 )
                 .queue();
     }
 
-//    public void onReady(ReadyEvent event) {
-//        event.getJDA().updateCommands()
-//                .addCommands(
-//                        Commands.slash("ping", "Ping?"),
-//                        Commands.slash("pong", "Pong?")
-//                )
-//                .queue();
-//    }
-//    @Override
-    public void onSlashCommand (SlashCommandInteractionEvent event)
+    @Override
+    public void onSlashCommandInteraction (SlashCommandInteractionEvent event)
     {
         switch (event.getName())
         {
-            case "ping"
-                -> event.reply("pong").queue();
-            case "pong"
-                -> event.reply("ping").queue();
-            default
-                -> event.reply("wat?").queue();
+            case "ping":
+                long created = event.getTimeCreated().toInstant().toEpochMilli();
+                long current = Instant.now().toEpochMilli();
+                event.reply((abs(current - created) % 1000) + " ms").queue();
+                break;
+            default:
+                event.reply("Me don't know that").queue();
         }
     }
-//    @Override
-//    public void onMessageReceived(MessageReceivedEvent event)
-//    {
-//        if (event.getAuthor().isBot()) return;
-//        // We don't want to respond to other bot accounts, including ourself
-//        Message message = event.getMessage();
-//        String content = message.getContentRaw();
-//        // getContentRaw() is an atomic getter
-//        // getContentDisplay() is a lazy getter which modifies the content for e.g. console view (strip discord formatting)
-//        if (content.equals("!ping"))
-//        {
-//            MessageChannel channel = event.getChannel();
-//            channel.sendMessage("Pong!").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
-//        }
-//    }
 }
